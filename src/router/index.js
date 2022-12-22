06c1105dff1f4c29a98ddb4e4b123c9c6d63d34d
Adit Tanu <adiettanu27@gmail.com>
2022-12-23 00:05:23 +0700

    route link param

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
 import JobsView from "../views/jobs/JobsView.vue"
+import JobDetailsView from "../views/jobs/JobDetailsView.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,12 @@ const router = createRouter({
             path: "/jobs",
             name: "Jobs",
             component: JobsView,
+        },
+        {
+            path: "/jobs/:id",
+            name: "JobDetails",
+            component: JobDetailsView,
+            props: true,
         },
     ],
 })
